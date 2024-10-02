fde861e12ea393d00d7b9d54b09aa1f48ec467b3
Jacob Isbell <jwisbell.astro@gmail.com>
2024-10-02 13:38:11 -0700

    Initial commit

---
+++ scripts/load_files.py
@@ -0,0 +1,23 @@
+"""
+Author: Jacob Isbell 
+Date: October 1, 2024
+
+Script which contains functions to load MATISSE fits files and turn them into dictionaries for later usage.
+This will unify data formatting.
+
+Specifically
+1. Loads TARGET_RAW_INT/CALIB_RAW_INT 
+2. Loads OPD files
+3. Loads photometric files
+"""
+
+def load_raw_int(fname):
+    """
+    If fname is a single file load it, if fname is an array of files, load each.
+    In both cases, return a data_dictionary (or pandas dataframe)
+    """
+
+    return None
+
+def load_opd(fname):
+    return None
