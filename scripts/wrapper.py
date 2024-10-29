d6f50e428b60e55798b2472ab3fe1e62aadb19ff
Jacob Isbell <jisbell@matisse-oca.oca.eu>
2024-10-29 05:36:48 +0100

    first test on the real server

--- scripts/wrapper.py
+++ scripts/wrapper.py
@@ -7,20 +7,23 @@ Script which contains primary logic for MATISSE QA.
 
 import json 
 from glob import glob
-from icecream import ic
+#from icecream import ic
 
 from load_files import load_raw_int
 from vis2_plots import plot_vis 
 from t3_plots import plot_cphase
+from sys import argv
 
 if __name__ =="__main__":
-    with open('config.json','r') as f:
+    script, configfile = argv
+    with open(configfile,'r') as f:
         cf = json.load(f)
     data_dir = cf['data_dir']
     output_dir = cf['output_dir']
     verbose = int(cf['verbose']) > 0
 
-    directories = glob(data_dir + '/*.rb')
+    directories = glob(data_dir + '/*raw_estimates*.rb')
+    print(directories,data_dir)
     for d in directories:
         files = glob(f'{d}/*RAW_INT_00*.fits')
         print('\n\n' + '#'*128)
@@ -32,10 +35,10 @@ if __name__ =="__main__":
         except:
             print(f'No files found in {d}')
             continue
-        
+        #TODO: using the tpl information, make subdirs in the output dir 
         try:
             print(f'Plotting visibilities ... ')
-            plot_vis(data_dict['vis'], save_fig=False, output_dir=None, verbose=verbose)
+            plot_vis(data_dict['vis'], save_fig=True, output_dir=output_dir, verbose=verbose)
             print(f'Visibilities plotted successfully!')
         except UnboundLocalError:
             print(f'Something wrong with {d} when plotting visibilities')
@@ -43,7 +46,7 @@ if __name__ =="__main__":
 
         try:
             print('Plotting closure phases ...')
-            plot_cphase(data_dict['cphase'], output_dir=None, save_fig=False, verbose=verbose)
+            plot_cphase(data_dict['cphase'], output_dir=output_dir, save_fig=True, verbose=verbose)
             print(f'Closure phases plotted successfully!')
         except:
             print(f'Something wrong with {d} when plotting closure phases')
